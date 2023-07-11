bf6ce108bf8af1b2141751b0fe7b14db4abab409
kush Muhamad <kushmohammed98@yahoo.com>
2023-07-11 18:42:31 +0300

    final

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,3 +1,48 @@
 class InstructorsController < ApplicationController
-   
-end
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
+    
+        def index
+            instructors =Instructor.all
+            render json: instructors
+        end
+    
+        def create
+            instructors =Instructor.create!(instructor_params)
+            render json: instructors
+        end
+    
+        def update
+            instructors = find_instructor
+            instructors.update(instructor_params)
+            render json: instructors
+        end
+    
+        def destroy
+            instructors = find_instructor
+            instructors.destroy
+            head :no_content
+        end
+    
+    
+    
+    
+        private
+    
+        def find_instructor
+           instructors =Instructor.find(params[:id])
+        end
+    
+        def instructor_params
+            params.permit(:name)
+        end
+    
+        def render_not_found_response
+            render json: { error: "Instructor not found" }, status: :not_found
+        end
+    
+        def render_invalid_response(invalid)
+            render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+        end
+    end
+    
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,47 @@
 class StudentsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
 
     def index
         students = Student.all
         render json: students
+    end
 
+    def create
+        students = Student.create!(student_params)
+        render json: students
+    end
+
+    def update
+        student = find_student
+        student.update(student_params)
+        render json: student
+    end
+
+    def destroy
+        student = find_student
+        student.destroy
+        head :no_content
+    end
+
+
+
+
+    private
+
+    def find_student
+        student = Student.find(params[:id])
+    end
+
+    def student_params
+        params.permit(:name, :major, :age, :instructor_id)
+    end
+
+    def render_not_found_response
+        render json: { error: "Student not found" }, status: :not_found
+    end
+
+    def render_invalid_response(invalid)
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
     end
 end
